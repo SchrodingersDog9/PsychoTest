0ad1ba4899f90c10bb904ef47a3cfa98019b7360
王翀 <wangchong@wangchongdembp.dhcp.wustl.edu>
2017-10-27 23:51:24 -0500

    Initial Commit

---
+++ PsychoTest/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  PsychoTest
+//
+//  Created by 王翀 on 2017/10/27.
+//  Copyright © 2017年 王翀. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
